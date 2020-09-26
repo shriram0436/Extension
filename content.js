589728b407620d48275af4899fdcf4ebccab6da2
shriram salunkhe <shriramsalunkhe0436@gmail.com>
2020-09-26 10:17:16 +0530

    using DOM links

--- content.js
+++ content.js
@@ -1,30 +1,65 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
     if (request == "twitter") {
-        const re = new RegExp('https://twitter.com/', 'gi')
-        const matches = document.documentElement.innerHTML.match(re)
-        console.log(matches);
+        //using regular expression
+        // const re = new RegExp('https://twitter.com/', 'gi')
+        // const matches = document.documentElement.innerHTML.match(re)
+        // console.log(matches);
+        // sendResponse({
+        //     count: matches,
+        //     len:matches.length
+        // })
+        //usinf DOM parsing
+        var x = document.links;
+        var txt=[];
+        var i;
+        for (i = 0; i < x.length; i++) {
+            txt.push(x[i].href);
+        }
+        console.log(txt);
         sendResponse({
-            count: matches,
-            len:matches.length
+            count: txt,
+            len:txt.length
         })
     }
 
     if (request == "facebook") {
-        const re = new RegExp('https://www.facebook.com/', 'gi');
-        const matches = document.documentElement.innerHTML.match(re)
+        // const re = new RegExp('https://www.facebook.com/', 'gi');
+        // const matches = document.documentElement.innerHTML.match(re)
+        // sendResponse({
+        //     count: matches,
+        //     len:matches.length
+        // })
+        var x = document.links;
+        var txt=[];
+        var i;
+        for (i = 0; i < x.length; i++) {
+            txt.push(x[i].href);
+        }
+        console.log(txt);
         sendResponse({
-            count: matches,
-            len:matches.length
+            count: txt,
+            len:txt.length
         })
     }
 
     if (request == "linkedin") {
-        const re = new RegExp('https://www.linkedin.com/', 'gi');
-        const matches = document.documentElement.innerHTML.match(re)
+        // const re = new RegExp('https://www.linkedin.com/', 'gi');
+        // const matches = document.documentElement.innerHTML.match(re)
+        // sendResponse({
+        //     count: matches,
+        //     len:matches.length
+        // })
+        var x = document.links;
+        var txt=[];
+        var i;
+        for (i = 0; i < x.length; i++) {
+            txt.push(x[i].href);
+        }
+        console.log(txt);
         sendResponse({
-            count: matches,
-            len:matches.length
+            count: txt,
+            len:txt.length
         })
     }
 
